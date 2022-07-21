65048d0e5689d2d8886159575114bfa5929517a6
Jeehee77 <gnarled47@gmail.com>
2022-07-21 22:35:10 +0900

    Storybook test

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Button from "./Button";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 export default {
   component: Button,
   title: "Button",
-};
+} as ComponentMeta<typeof Button>;
 
-const Template = ({ args }: { args: any }) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Default = Template.bind({});
+Default.args = {
+  text: "TEST",
+  state: "defualt",
+};
 
 export const Active = Template.bind({});
